af7acb8f751d13d87eeecc31fec6b7644fcb8870
CYUNQ <cuiyunqi@yahoo.com>
2021-07-24 23:14:44 +1000

    Default Changelist

---
+++ src/com/cyunq/huffmantree/Node.java
@@ -0,0 +1,36 @@
+package com.cyunq.huffmantree;
+
+/**
+ * 实现Comparable 接口 达成排序的作用
+ */
+public class Node implements Comparable<Node>{
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int value){
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" + "value=" + value + '}';
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        //表示从小到大排序
+        return this.value - o.value;
+    }
+
+    public void preOrder(){
+        System.out.println(this);
+
+        if (this.left != null){
+            this.left.preOrder();
+        }
+        if (this.right != null){
+            this.right.preOrder();
+        }
+    }
+}
