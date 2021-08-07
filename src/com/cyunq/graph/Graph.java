68714e518c07f1992f24ed61786851ac15166580
CYUNQ <cuiyunqi@yahoo.com>
2021-08-07 15:06:10 +1000

    Default Changelist

--- src/com/cyunq/graph/Graph.java
+++ src/com/cyunq/graph/Graph.java
@@ -1,13 +1,14 @@
 package com.cyunq.graph;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Graph {
     private ArrayList<String> vertexList;//图的顶点
     private int[][] edges;//图的边
     private int numOfEdges;//表示边的数目
+    private boolean[] isVisited;
 
     public Graph(int n) {
         //初始化矩阵和vertexList
@@ -33,8 +34,6 @@ public class Graph {
         this.edges = edges;
     }
 
-
-
     public void setNumOfEdges(int numOfEdges) {
         this.numOfEdges = numOfEdges;
     }
@@ -49,6 +48,8 @@ public class Graph {
     }
 
     /**
+     * 添加边
+     *
      * @param v1     顶点一的下标
      * @param v2     顶点二的下标
      * @param weight 默认为0，v1 与 v2 有链接时为1
@@ -74,17 +75,125 @@ public class Graph {
         return numOfEdges;
     }
 
-    public String getValueByIndex(int i){
+    public String getValueByIndex(int i) {
         return vertexList.get(i);
     }
 
-    public int getWeight(int v1, int v2){
+    public int getWeight(int v1, int v2) {
         return edges[v1][v2];
     }
 
-    public void showGraph(){
-        for (int[] link : edges){
+    public void showGraph() {
+        System.out.println();
+        System.out.println();
+        for (int[] link : edges) {
             System.out.println(Arrays.toString(link));
+        }
+        System.out.println();
+    }
+
+    /**
+     * 返回当前节点的下一个节点的下标
+     *
+     * @param index 当前节点在ArrayList中的下标
+     * @return 返回下一个邻接节点的下标
+     */
+    public int getNext(int index) {
+
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     *
+     * @param v1 当前节点下标
+     * @param v2 前一个邻接节点下标
+     * @return 下一个邻接节点下标
+     */
+    public int getNextAfter(int v1, int v2) {
+
+        for (int j = v2 + 1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0 ){
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 深度优先遍历算法
+     */
+    private void dfs(boolean[] isVisited, int i){
+        //首先访问该节点
+        System.out.print(getValueByIndex(i) + " -> ");
+        isVisited[i] = true;
+
+        int w = getNext(i);
+        while (w != -1){
+            if (!isVisited[w]){
+                dfs(isVisited,w);
+            }
+            w = getNextAfter(i, w);
+        }
+    }
+
+    /**
+     * @override 重载dfs算法
+     */
+    public void dfs(){
+        isVisited = new boolean[vertexList.size()];
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]){
+                dfs(isVisited,i);
+            }
+        }
+    }
+
+    /**
+     * 广度优先算法
+     * @param isVisited
+     * @param i
+     */
+    private void bfs(boolean[] isVisited, int i){
+        int u;//表示队列头结点的下标
+        int w;//表示邻接节点的下标
+        //记录节点访问顺序的队列
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        //访问节点
+        System.out.print(getValueByIndex(i) + " -> ");
+        isVisited[i] = true;
+        //将节点加入队列
+        queue.addLast(i);
+
+        while (!queue.isEmpty()){
+            //取出队列头结点下标
+            u =  queue.removeFirst();
+            //取出第一个邻接节点的下标
+            w = getNext(u);
+            while (w != -1){
+                if (!isVisited[w]){
+                    System.out.print(getValueByIndex(w) + " -> ");
+                    isVisited[w] = true;
+                    queue.addLast(w);
+                }
+                w = getNextAfter(u,w);//提现广度优先的地方
+            }
+        }
+    }
+
+    /**
+     * 遍历所有的节点，都进行广度优先
+     */
+    public void bfs(){
+        isVisited = new boolean[vertexList.size()];
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]){
+                bfs(isVisited,i);
+            }
         }
     }
 }
--- src/com/cyunq/graph/GraphDemo.java
+++ src/com/cyunq/graph/GraphDemo.java
@@ -2,14 +2,25 @@ package com.cyunq.graph;
 
 public class GraphDemo {
     public static void main(String[] args) {
-        Graph graph = new Graph(5);
 
-        String vertexValue[] = {"A","B","C","D","E"};
+        String[] vertexValue = {"A","B","C","D","E"};
+        testGraph1(vertexValue);
 
+        String[] vertexValue1 = {"1","2","3","4","5","6","7","8"};
+        testGraph2(vertexValue1);
+    }
+
+    private static void addVertex(Graph graph, String[] vertexValue){
         //添加顶点
         for (String value : vertexValue){
             graph.insertVertex(value);
         }
+    }
+
+    private static void testGraph1(String[] vertexValue){
+
+        Graph graph = new Graph(vertexValue.length);
+        addVertex(graph,vertexValue);
 
         //添加边
         //A-B, A-C, B-C, B-D, B-E
@@ -20,5 +31,40 @@ public class GraphDemo {
         graph.insertEdges(1,4,1);
 
         graph.showGraph();
+        System.out.println("深度优先遍历");
+        graph.dfs();
+
+        System.out.println();
+        System.out.println();
+        System.out.println("广度优先遍历");
+        graph.bfs();
+    }
+
+    /**
+     * 测试深度与广度优先的具体区别
+     * @param vertexValue 顶点数组
+     */
+    private static void testGraph2(String[] vertexValue){
+        Graph graph = new Graph(vertexValue.length);
+        addVertex(graph,vertexValue);
+
+        //添加边
+        graph.insertEdges(0, 1, 1);
+        graph.insertEdges(0, 2, 1);
+        graph.insertEdges(1, 3, 1);
+        graph.insertEdges(1, 4, 1);
+        graph.insertEdges(3, 7, 1);
+        graph.insertEdges(4, 7, 1);
+        graph.insertEdges(2, 5, 1);
+        graph.insertEdges(2, 6, 1);
+        graph.insertEdges(5, 6, 1);
+
+        graph.showGraph();
+        System.out.println("深度优先遍历");
+        graph.dfs();
+        System.out.println();
+        System.out.println();
+        System.out.println("广度优先遍历");
+        graph.bfs();
     }
 }
